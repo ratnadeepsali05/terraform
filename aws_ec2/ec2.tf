b50c93b30a0b6f6e756f46a270ff418c79fd3c0e
Ratnadeep sali <dipaksali767@gmail.com>
2024-10-28 11:38:36 +0530

    o

--- aws_ec2/ec2.tf
+++ aws_ec2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this_inst" {
     ami = "ami-0c8eea98010057bd0" 
     disable_api_stop  = true 
     disable_api_termination = true  
-    instance_type = "t2.micro" 
+    instance_type = "t4g.nano" 
     key_name = "mum-key"
     security_groups = ["sg-0a915226a5d9bb382"]
     count = 4  #loop 
