f5a55cae7dccc330feca07a8e6c0bb7a12211a29
Ratnadeep sali <dipaksali767@gmail.com>
2024-10-28 11:01:35 +0530

    region

--- aws_ec2/ec2.tf
+++ aws_ec2/ec2.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "this_ubuntu" {
     disable_api_stop  = true 
     disable_api_termination = true  
     instance_type = "t2.micro" 
-    key_name = "mumbaianupb38kp"
-    security_groups = [sg-0206fe395dc0f6d38]
+    key_name = "mum-key"
+    security_groups = ["sg-0a915226a5d9bb382"]
     count = 4  #loop 
     tags = {
       purpose = "jenkins server"
