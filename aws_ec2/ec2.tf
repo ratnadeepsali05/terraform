5f9abf5cfa17bba7e823d5555247ebf551b1c6ba
Ratnadeep sali <dipaksali767@gmail.com>
2024-10-28 11:19:50 +0530

    sg

--- aws_ec2/ec2.tf
+++ aws_ec2/ec2.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "this_ubuntu" {
+resource "aws_instance" "this_inst" {
 
-    ami = "ami-0325498274077fac5" 
+    ami = " ami-0c8eea98010057bd0" 
     disable_api_stop  = true 
     disable_api_termination = true  
     instance_type = "t2.micro" 
