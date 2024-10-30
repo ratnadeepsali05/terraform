4ff9c4cd6ff5a7965ca4d7bb38f7dba95ea9b4c1
Ratnadeep sali <dipaksali767@gmail.com>
2024-10-30 20:15:25 +0530

    s3

--- s3_bucket/s3_main.tf
+++ s3_bucket/s3_main.tf
@@ -6,10 +6,10 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
  
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_bucket_object" "object_s3" {
   bucket = "aws_s3_bucket.bucket.id"
-  key    = "object"
-  source = "ratnadeepsali05/terraform/s3_bucket/object"
+  key    = "index.html"
+  source = "ratnadeepsali05/terraform/s3_bucket/object/index.html"
 
 
 }
