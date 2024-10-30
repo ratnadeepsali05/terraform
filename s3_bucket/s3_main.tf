9eb12afbe97699a0abf172e69c8b7f744c44e280
Ratnadeep sali <dipaksali767@gmail.com>
2024-10-30 20:22:57 +0530

    aci

--- s3_bucket/s3_main.tf
+++ s3_bucket/s3_main.tf
@@ -9,7 +9,8 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_object" "object_s3" {
   bucket = "aws_s3_bucket.bucket.id"
   key    = "index.html"
-  source = "ratnadeepsali05/terraform/s3_bucket/object/index.html"
+  source = "ratnadeepsali05/terraform/s3_bucket/object/"
+  acl    = "public-read"
 
 
 }
