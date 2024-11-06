db04d8fa7ef40000944ecaebbb952278c143e03d
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-06 12:13:58 +0530

    git

--- RDS/variables.tf
+++ RDS/variables.tf
@@ -1,5 +1,5 @@
 variable "this_list" {
     type = list 
-    default = ["20" , "mysql" , "8.0" , "db.t3.micro" , "mydatabase" , "admin" , "password123" , "default.mysql8.0" , "true" , "true" , "sg-0a915226a5d9bb382" , "false"]
+    default = ["20" , "mysql" , "8.0" , "db.t3.micro" , "mydatabase_mysql_1" , "admin" , "password123" , "default.mysql8.0" , "true" , "true" , "sg-0a915226a5d9bb382" , "false"]
      
 }
