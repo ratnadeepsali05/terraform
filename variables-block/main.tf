b671004f7f3321b2bd007e2668a8d498b2ccdd46
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-06 11:34:53 +0530

    var

--- variables-block/main.tf
+++ variables-block/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "this_ubuntu" {
-    ami = var.this_image_id 
-    disable_api_stop  = var.this_disable_api_stop 
-    disable_api_termination = var.this_disable_api_termination  
-    instance_type = var.this_list[0]
-    #key_name=""
-    vpc_security_group_ids = [var.this_vpc_security_group_ids]
-    count = var.this_count  #loop #var.this_list[1]
+    ami = var.this_list[0] 
+    disable_api_stop  = var.this_list[1] 
+    disable_api_termination = var.this_list[2]  
+    instance_type = var.this_list[3]
+    key_name= var.this_list[4]
+    vpc_security_group_ids = [var.this_list[5]]
+    count = var.this_list[6]
     tags = {
       purpose = "webserver"
     } 
