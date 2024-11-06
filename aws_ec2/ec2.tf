ed0162ec38c1833ae35aac98d4ec5e43301dc539
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-06 11:11:12 +0530

    var

--- aws_ec2/ec2.tf
+++ aws_ec2/ec2.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "this_inst" {
-    ami = "ami-0c8eea98010057bd0" 
-    disable_api_stop  = true 
-    disable_api_termination = true  
+    ami = "ami-09b0a86a2c84101e1"   
     instance_type = "t4g.nano" 
     key_name = "mum-key"
+    vpc_security_group_ids = ["sg-0a915226a5d9bb382"]
     count = 4  #loop 
     tags = {
       name = "ratnadeep sali"
