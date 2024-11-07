8bf8e68bef0b3d3fdabe6829566e3c42867d79a8
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-07 21:37:27 +0530

    db

---
+++ DynamoDB/main.tf
@@ -0,0 +1,26 @@
+resource "aws dynamodb table" "basic-table-" {
+
+  name          = "dynamo_table"
+  billing_mode  = "PROVISIONED"
+  read capacity = 20
+  write_capacity = 20
+  hash_key       = "table_demo_id"
+
+  attribute {
+    name = "table_demo_id"
+    type = "S"
+
+}
+
+ttl {
+  ttribute name = "TimeToExist"
+  enabled       = false
+}
+
+tags = {
+Name = "Sample Table
+Environment = "Test"
+
+ }
+
+}
