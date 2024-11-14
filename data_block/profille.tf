a242a1f08f8aafb10e49464dc79b056f46bdca26
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-14 19:42:10 +0530

    test

--- data_block/profille.tf
+++ data_block/profille.tf
@@ -8,19 +8,3 @@ provider "aws" {
                 }
     }
 }
-
-terraform {
-
-required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.66.1"
-    }
-  }
-
-backend "s3" {
-	bucket = "terraform-state-bucket-s3-state-store" 
-	key = "terraform.tfstate"
-	region = "ap-south-1"
-}
-}
