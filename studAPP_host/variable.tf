6ae1cc36f6e2142d607f89f9af82921ca38037bc
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-14 18:37:48 +0530

    test

--- studAPP_host/variable.tf
+++ studAPP_host/variable.tf
@@ -1,9 +1,11 @@
 variable "ubuntu_ami" {
   description = "AMI for Ubuntu instance"
+  type        =  string
 }
 
 variable "aws_instance_type" {
   description = "Instance type for EC2"
+  type        = string
 }
 
 variable "key_name" {
