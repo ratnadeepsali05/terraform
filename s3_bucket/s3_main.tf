97a4ac53b3cf93a715c159f51b809d1b40719821
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-18 12:49:03 +0530

    test

--- s3_bucket/s3_main.tf
+++ s3_bucket/s3_main.tf
@@ -6,9 +6,9 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
  
-resource "aws_s3_bucket_object" "my_object" {
+/*resource "aws_s3_bucket_object" "my_object" {
   bucket = aws_s3_bucket.my_bucket.bucket                   # Reference the bucket created above
   key    = "ratnadeepsali05/terraform/s3_bucket/object"     # Path and name for the object in the bucket
   source = "ratnadeepsali05/terraform/s3_bucket/object"      # Local path to the file you want to upload
   acl    = "public-read"                                     # Optional: Set ACL, e.g., public-read for public access
-}
+}*/
