7a5c1753f64309485d43dcb9666058c487f77b7b
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-19 11:56:18 +0530

    pull

--- module_block/prod/module.tf
+++ module_block/prod/module.tf
@@ -1,23 +1,20 @@
 module "ec2" {
-        source = "/home/cloudshell-user/terraform/module_block/resource/ec2"
-        This_api_termination = true
-        This_key = "mum-key"
-        This_az  = "ap-south-1a"
-        This_tag =  "Ratnadeep"
-        This_inst = "t2.micro"
-        #This_sg  = ["sg-09d5d9601120f72dd"]
-        This_ami = "ami-09b0a86a2c84101e1"
-        this_aws_instance_subnet = module.aws_vpc.subnet_id
-        
-
+    source = "/home/anup/terraform/deleteafterpractice/Module/Resources/EC2"
+    this_image_id = "ami-09b0a86a2c84101e1"
+    this_list = "t2.micro"
+    this_disable_api_stop = false
+    this_disable_api_termination = false
+    #this_vpc_security_group_ids = "sg-0505874879e7ce6cd"
+    this_aws_instance_subnet = module.vpc.subnet_id
+    #this_sg_vpc_id = module.vpc.vpc_id
+    //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
 }
 
-module "aws_vpc" {
-        source = "/home/cloudshell-user/terraform/module_block/resource/vpc"
-        this_vpc_cidr_block = "192.168.0.0/16"
-        this_vpc_cidr_tags = "vpc"
-        this_public_cidr_block  = "192.168.0.0/17"
-        this_public_map_pub = true
-        this_public_tags = "my_public"
-        this_vpc_az = "ap-south-1a"
-}
+module "vpc" {
+     source = "/home/anup/terraform/deleteafterpractice/Module/Resources/VPC"
+     this_vpc_cidr_block = "192.168.0.0/16"
+     this_vpc_tags = "this_vpc"
+     this_subnet_pub_cidr_block = "192.168.0.0/17"
+     this_subnet_pub_map_ip  = true 
+     this_subnet_pub_tags = "pub_subnet"
+     this_vpc_az = "ap-south-1a"
