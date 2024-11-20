95b7238281b13ccd465881b96333d93dde0cdf05
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-20 22:04:05 +0530

    test

--- module_block/prod/module.tf
+++ module_block/prod/module.tf
@@ -1,13 +1,14 @@
 module "ec2" {
     source = "/home/cloudshell-user/terraform/module_block/resource/ec2"
-    this_image_id = "ami-09b0a86a2c84101e1"
+    this_This_ami = "ami-09b0a86a2c84101e1"
     this_list = "t2.micro"
+    This_key  = "mum-key"
     this_disable_api_stop = false
     this_disable_api_termination = false
     #this_vpc_security_group_ids = "sg-0505874879e7ce6cd"
     this_aws_instance_subnet = module.aws_vpc.subnet_id
-    #this_sg_vpc_id = module.vpc.vpc_id
-    //this_aws_vpc_id = module.vpc.aws_vpc_id
+    this_sg_vpc_id = module.vpc.vpc_id
+    //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
 }
 
 module "aws_vpc" {
