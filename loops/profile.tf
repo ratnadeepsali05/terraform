75b13a4d1f2f2cda5086e124e660a80d9869a0cf
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-22 11:13:52 +0530

    test

--- loops/profile.tf
+++ loops/profile.tf
@@ -8,3 +8,22 @@ provider "aws" {
                 }
     }
 }
+
+terraform {
+
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
+    }
+  }
+
+backend "s3" {
+	bucket = "mytfmainbucketgloble5525" 
+	key = "terraform.tfstate"
+	region = "ap-south-1"
+	dynamodb_table = "test-my-app-table-d"
+  profile = "configs"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
+}
