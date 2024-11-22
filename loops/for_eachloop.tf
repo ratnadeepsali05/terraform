0b176647bee6643fa20063efddbee6cce93de83b
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-22 11:28:06 +0530

    test

--- loops/for_eachloop.tf
+++ loops/for_eachloop.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "this_aws_instance" {
 
 variable aws_ami {
        type = list(string)
-       default = ["ami-09b0a86a2c84101e1", "ami-0aebec83a182ea7ea", "ami-022ce6f32988af5fa" , "ami-04a81a99f5ec58529" ]
+       default = ["ami-09b0a86a2c84101e1", "ami-0aebec83a182ea7ea", "ami-022ce6f32988af5fa" , ]
 }
 
 
