e6adce1846f21caa18b0b4f594a28d91da411ea4
Ratnadeep sali <dipaksali767@gmail.com>
2024-11-24 18:10:56 +0530

    tet

--- provisioners_block/remote-exec/exec-remote_pro.tf
+++ provisioners_block/remote-exec/exec-remote_pro.tf
@@ -1,24 +1,32 @@
- 
-  resource "aws_instance" "this_aws_instance" {
-    //for_each = toset(var.imageid)
+provider "aws" {
+    region =  "us-west-2"
+    access_key = ""
+    secret_key = ""
+    //profile = "configs"
+}
+ resource "aws_instance" "this_aws_instance" {
     ami = "ami-0614680123427b75e"
     vpc_security_group_ids = ["sg-0a915226a5d9bb382"]
     key_name = "mum-key"
     instance_type = "t2.micro"
- 
-    connection {
+     
+     provisioner "file" {
+    source      = "readme.md"
+    destination = "/home/ec2-user/readme.md"
+      connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("${path.module}/id_rsa.pem")
+    private_key = file("${path.module}/id_rsa")
     host     = "${self.public_ip}"
+  } 
   }
-
-  provisioner "remote-exec" {
+ provisioner "remote-exec" {
     inline = [
-      "yum install httpd -y",
-      "systemctl start httpd",
-      "systemctl enable httpd"
+      "ifconfig > /tmp/ifconfig.out",
+      "echo 'hello world' > /tmp/test.txt",
     ]
   }
-    
-}  
+
+
+ }
+ 
