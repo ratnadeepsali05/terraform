c8c6915dca4fe3f51e0e65eb1d9dc853d81aca37
Ratnadeep sali <dipaksali767@gmail.com>
2025-01-10 17:29:48 +0530

    output

--- staticwebhosting/output.tf
+++ staticwebhosting/output.tf
@@ -1,8 +1,13 @@
 
-output "instance_domain_name" {
+/*output "instance_domain_name" {
   value = aws_route53_record.carvcilla_dns_record.name
-}
+}*/
 
 output "instance_id" {
-  value = aws_instance.app_server.id
+  value = aws_instance..id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.carvcilla_instance.public_ip
 }
