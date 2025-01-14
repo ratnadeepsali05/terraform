b0f2c1f1ec9c7c11f3b6d5a05b6ed0d000136f23
Ratnadeep sali <dipaksali767@gmail.com>
2025-01-14 23:27:17 +0530

    ls

--- EKS/main.tf
+++ EKS/main.tf
@@ -3,11 +3,12 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = "my-eks-cluster"
+  ode_group_name = "my_node_gls"
   cluster_version = "1.31" # Latest EKS version at the time of writing
   subnet_ids      = ["subnet-0475abf0fd6adfce0","subnet-05d2056f662a6dc9f","subnet-01f99de82fab8114b"]
   vpc_id          = "vpc-036309236ea8db490"
 
-  managed_node_groups = {
+  managed_node_group = {
     eks_nodes = {
       desired_capacity = 2
       max_capacity     = 3
